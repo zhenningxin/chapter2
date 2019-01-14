4d6bb1ee49fb198ec8c1596926b10ba31dd6ebd9
zhenningxin <guanxin1828@gmail.com>
2019-01-14 12:58:47 +0800

    第二章

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int number = 15;
+            number = number + 10;
+            number = 36 * 15;
+            number = 12 - (42 / 7);
+            number += 10;
+            number *= 3;
+            number = 71 / 3;
+
+            int count = 0;
+            count++;
+            count--;
+
+            string result = "hello";
+            result += "again" + result;
+            output.Text = result;
+            result = "the value is:" + count;
+            result = "";
+
+            bool yesNo = false;
+            bool anotherBool = true;
+            yesNo = !anotherBool;
+
+
+
+        }
+    }
+}
